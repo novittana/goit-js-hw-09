06a25193f644902b796b090447485ff931c1e2a1
novittana <107628111+novittana@users.noreply.github.com>
2023-01-16 23:09:25 +0000

    Deploying to gh-pages from @ novittana/goit-js-hw-09@808143ff3f2d0dacdeba635c86eaf06bc837925f 🚀

--- 03-promises.19103ff6.js
+++
@@ -1,2 +0,0 @@
-({formEl:document.querySelector(".form")}).formEl.addEventListener("submit",(e=>{e.preventDefault;e.target.elements.delay.value,e.target.elements.step.value,e.target.elements.amount.value}));
-//# sourceMappingURL=03-promises.19103ff6.js.map
--- 03-promises.ccd86dba.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault,e.target.elements.delay.value,e.target.elements.step.value,e.target.elements.amount.value}));
-//# sourceMappingURL=03-promises.ccd86dba.js.map
